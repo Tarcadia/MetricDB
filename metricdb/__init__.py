4addacabe9c2fe7ff7754d589d09a05af0a0d812
Tarcadia <tarcadia@qq.com>
2025-02-23 00:45:49 +0800

    Add package info

--- metricdb/__init__.py
+++ metricdb/__init__.py
@@ -1,6 +1,12 @@
 
 # -*- coding: UTF-8 -*-
 
+
+__author__ = "Tarcadia"
+__url__ = "https://github.com/Tarcadia/MetricDB"
+__copyright__ = "Copyright 2024-2025"
+__credits__ = ["Tarcadia"]
+__license__ = "MIT"
 __version__ = "0.1.0"
 
 
